4760a7ddc2f22aae89a52e299b4c3815c849279f
mixxamm <maximjanssensj5@gmail.com>
2020-01-19 17:29:53 +0100

    Maak form met invoer naar CouchDB
    Co-authored-by: Collette Cédric [student] <s107601@ap.be>
    Co-authored-by: s107571@ap.be <s107571@ap.be>
    Co-authored-by: Dana Tabatabaie Irani <36732673+MrDanaT@users.noreply.github.com>
    Co-authored-by: dtkturpal <dtkturpal@gmail.com>
    Co-authored-by: Vergracht Jorg [student] <jorg.vergracht@student.ap.be>
    Co-authored-by: Ongenae Pieter [student] <s106707@ap.be>

---
+++ src/Form.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Form = props => {
+
+    const submit = (event) => {
+        event.preventDefault();
+        let input = {};
+        for (let i in event.target.elements) {
+            let ele = event.target.elements[i];
+            if (ele.tagName != "INPUT") continue;
+
+            console.log(ele.name, ele.value);
+            input[ele.name] = ele.value;
+        }
+        
+        fetch(`http://localhost:5984/producten/${input.naam}`, {method: "PUT", body: JSON.stringify(input)}).then(response => response.json()).then(json => console.log(json))
+    };
+
+    return(
+        <div>
+            <form onSubmit={submit}>
+                <input type="text" name="naam" placeholder="naam"></input>
+                <input type="number" name="aantal" min="1" defaultValue="1"></input>
+                <input type="text" name="prijs" placeholder="prijs"></input>
+                <input type="date" name="datum"></input>
+                <button type="submit">Invoeren</button>
+            </form>
+        </div>
+    );
+}
